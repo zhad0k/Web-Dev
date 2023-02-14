eadb6a955b2ab628256f5b142ac55e98eeb9cc36
Zhadyra <98956370+zhad0k@users.noreply.github.com>
2023-02-14 09:15:31 -0800

    Delete Data types_Storing quotes.js

--- Data types_Storing quotes.js
+++
@@ -1,7 +0,0 @@
-let name = "Ilya";
-
-alert( `hello ${1}` ); // hello 1//here is a number
-
-alert( `hello ${"name"}` ); // hello name//here is a string
-
-alert( `hello ${name}` ); // hello Ilya//here is a variable connected to the first written
