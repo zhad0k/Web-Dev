a7d65b3eea93170b70f91bdfecdf9a24aafb88c5
Zhadyra <98956370+zhad0k@users.noreply.github.com>
2023-02-14 09:16:41 -0800

    Delete Logical operators_A question about "if".js

--- Logical operators_A question about "if".js
+++
@@ -1,3 +0,0 @@
-if (-1 || 0) alert( 'first' ); //runs because for || it needs first truly value
-if (-1 && 0) alert( 'second' );// do not work cause  for && it needs first falsy value
-if (null || -1 && 1) alert( 'third' );//yes work but first && then ||
